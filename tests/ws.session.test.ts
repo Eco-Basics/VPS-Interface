2ad797215e963b6a9619702f4d022c086be0d756
Eco-Basics <ecobasics0108@gmail.com>
2026-04-01 20:34:57 +0530

    test(02-01): add ws.session test scaffold with 5 locked requirement stubs
    - TERM-01: PTY data forwarding to client
    - TERM-02: input message writes to PTY
    - TERM-03: resize message handling
    - SESS-03: reconnect buffered output replay
    - SESS-05: exit notification broadcast and WS close

---
+++ tests/ws.session.test.ts
@@ -0,0 +1,7 @@
+describe('session WebSocket bridge', () => {
+  test.todo('TERM-01: forwards PTY data to the client as a raw string');
+  test.todo('TERM-02: writes input messages to the PTY');
+  test.todo('TERM-03: calls pty.resize on resize messages');
+  test.todo('SESS-03: reconnect replays buffered PTY output before live stream');
+  test.todo('SESS-05: broadcasts exit notification and closes WebSocket clients');
+});
